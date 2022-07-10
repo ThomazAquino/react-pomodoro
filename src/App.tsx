37d687148a20c6f7afdb3c4ac718197bd51013a6
ThomazAquino <thomazmanoel.aquino@gmail.com>
2022-07-10 20:10:33 -0300

    feat(chore): add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Button variant="primary" />
-    <Button variant="secondary" />
-    <Button variant="success" />
-    <Button variant="danger" />
-    <Button />
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
